4f466c08f21e8dabdd81ef0f7b03749d1dbe2d18
Kim Brannian <brannian@gmail.com>
2021-10-22 13:32:41 -0700

    Add code to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
-//
 $(document).ready(function() {
-  $(form#language).submit(function(event) {
+  $('form#language').submit(function(event) {
     event.preventDefault();
-  }
+    $('#lionfacts').hide();
+    $('#tigerfacts').hide();
+    $('#bearfacts').hide();
+    const firstQuestion= $("input:radio[name=prefer1]:checked").val();
+    const secondQuestion= $("input:radio[name=prefer2]:checked").val();
+    const thirdQuestion= $("input:radio[name=prefer3]:checked").val();
+    const fourthQuestion= $("input:radio[name=prefer4]:checked").val();
+    const fifthQuestion= $("input:radio[name=prefer5]:checked").val();
+    
 
-
-  
-  
-  }
+    if (thirdQuestion === "traditional" && (fourthQuestion === "webdev")) {
+      $('#csharp').show(); 
+    } else if (thirdQuestion === "startup" && (fourthQuestion === "webdev")) {
+      $('#python').show();
+    } else if (fourthQuestion === "mobile" && (fifthQuestion=== "ios")){
+      $('#swift').show();
+    }
+    
+      else {
+        $('#bearfacts').show();
+    }
+  }); 
 });
-}); 
-
-
 
 
