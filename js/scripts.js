b14f8e1d19d15bb3a43e3b1007c82167e4a01f2e
Kim Brannian <brannian@gmail.com>
2021-10-30 15:26:23 -0700

    Fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     $('#swift').hide();
     $('#java').hide();
 
-    const firstQuestion= $("input:radio[name=prefer1]:checked").val();
-    const secondQuestion= $("input:radio[name=prefer2]:checked").val();
-    const thirdQuestion= $("input:radio[name=prefer3]:checked").val();
-    const fourthQuestion= $("input:radio[name=prefer4]:checked").val();
-    const fifthQuestion= $("input:radio[name=prefer5]:checked").val();
+    const firstQuestion = $("input:radio[name=prefer1]:checked").val();
+    const secondQuestion = $("input:radio[name=prefer2]:checked").val();
+    const thirdQuestion = $("input:radio[name=prefer3]:checked").val();
+    const fourthQuestion = $("input:radio[name=prefer4]:checked").val();
+    const fifthQuestion = $("input:radio[name=prefer5]:checked").val();
     
 
     if (thirdQuestion === "traditional" && (fourthQuestion === "webdev")) {
@@ -22,8 +22,7 @@ $(document).ready(function() {
     } else if (fourthQuestion === "mobile" && (fifthQuestion=== "ios")){
       $('#swift').fadeToggle();
       $('#language').hide();
-    } 
-      else {
+    } else {
       $('#java').fadeToggle();
       $('#language').hide();
   }
