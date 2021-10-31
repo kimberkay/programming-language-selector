9a761ba9572d6e4de0cb79511805c35727984c65
Kim Brannian <brannian@gmail.com>
2021-10-31 14:37:23 -0700

    Edit scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
   $('form#language').submit(function(event) {
     event.preventDefault();
-    $('#csharp').hide();
-    $('#python').hide();
-    $('#swift').hide();
-    $('#java').hide();
-
+    $('#csharp','#python','#swift','java').hide();
+    
     const firstQuestion = $("input:radio[name=prefer1]:checked").val();
     const secondQuestion = $("input:radio[name=prefer2]:checked").val();
     const thirdQuestion = $("input:radio[name=prefer3]:checked").val();
     const fourthQuestion = $("input:radio[name=prefer4]:checked").val();
     const fifthQuestion = $("input:radio[name=prefer5]:checked").val();
     
-
     if (thirdQuestion === "traditional" && (fourthQuestion === "webdev")) {
       $('#csharp').fadeToggle();
       $('#language').hide(); 
@@ -26,9 +22,6 @@ $(document).ready(function() {
       $('#java').fadeToggle();
       $('#language').hide();
   }
-
-
-    
   }
   ); 
 });
